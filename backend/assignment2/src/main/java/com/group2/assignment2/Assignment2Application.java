e1243b501c02051d3948556171e3f4287e948cb7
Joelpettersson35 <joelpett101@student.kau.se>
2026-02-06 15:45:09 +0100

    springbootprojekt skapat

---
+++ backend/assignment2/src/main/java/com/group2/assignment2/Assignment2Application.java
@@ -0,0 +1,18 @@
+package com.group2.assignment2;
+
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class Assignment2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment2Application.class, args);
+	}{
+		log.info("hello world");
+	}
+
+}
---
+++ backend/assignment2/src/test/java/com/group2/assignment2/Assignment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group2.assignment2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
